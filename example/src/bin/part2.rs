aa5e6368f512cfb5fc9f7b864eae57396d0945e1
Moritz <manimoritz@gmail.com>
2024-12-08 13:31:17 +0100

    add clippy warnings to example template for better error handling

--- example/src/bin/part2.rs
+++ example/src/bin/part2.rs
@@ -1,3 +1,8 @@
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+#![deny(unused_must_use)]
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = part2(input);
