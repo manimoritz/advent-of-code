a1a6ce089affd13b74a0fabe31a914d12516f0db
Moritz <manimoritz@gmail.com>
2024-12-09 23:20:17 +0100

    advent-of-code 2024 day-06

--- 2024/day-06/src/bin/part1.rs
+++ 2024/day-06/src/bin/part1.rs
@@ -4,8 +4,84 @@ fn main() {
     dbg!(output);
 }
 
-fn part1(_input: &str) -> i32 {
-    todo!();
+fn part1(input: &str) -> i32 {
+    let mut matrix: Vec<Vec<(bool, bool)>> = Vec::new();
+    let mut current_position: (usize, usize) = (0, 0);
+    let mut direction: (i32, i32) = (0, 0);
+
+    for (i, l) in input.lines().enumerate() {
+        matrix.push(Vec::new());
+
+        for (j, c) in l.chars().enumerate() {
+            let mut obstacle = false;
+            let mut visited = false;
+
+            match c {
+                '^' => {
+                    direction = (-1, 0);
+                    visited = true;
+                    current_position = (i, j);
+                }
+                '>' => {
+                    direction = (0, 1);
+                    visited = true;
+                    current_position = (i, j);
+                }
+                'v' => {
+                    direction = (1, 0);
+                    visited = true;
+                    current_position = (i, j);
+                }
+                '<' => {
+                    direction = (0, -1);
+                    visited = true;
+                    current_position = (i, j);
+                }
+                '#' => {
+                    obstacle = true;
+                }
+                _ => {}
+            }
+
+            matrix[i].push((obstacle, visited));
+        }
+    }
+
+    let mut next_step = (
+        current_position.0 as i32 + direction.0,
+        current_position.1 as i32 + direction.1,
+    );
+    // While the next square is not out of frame
+    while next_step.0 >= 0
+        && (next_step.0 as usize) < matrix.len()
+        && next_step.1 >= 0
+        && (next_step.1 as usize) < matrix[current_position.0].len()
+    {
+        // Set the current square as visited
+        matrix[current_position.0][current_position.1].1 = true;
+
+        // Check if the next square is an obstacle
+        if matrix[next_step.0 as usize][next_step.1 as usize].0 {
+            direction = (direction.1, -direction.0);
+        } else {
+            // Update the position of the guard
+            current_position = (next_step.0 as usize, next_step.1 as usize);
+        }
+
+        // Update the next step of the guart
+        next_step = (
+            current_position.0 as i32 + direction.0,
+            current_position.1 as i32 + direction.1,
+        );
+    }
+
+    // Sum up all the visited squares plus 1 for the last square before leaving the frame
+    let result = matrix
+        .iter()
+        .map(|row| row.iter().filter(|entry| entry.1).collect::<Vec<_>>().len())
+        .sum::<usize>()
+        + 1;
+    result as i32
 }
 
 #[cfg(test)]
@@ -14,8 +90,17 @@ mod tests {
 
     #[test]
     fn test1() {
-        let test_case = "8";
+        let test_case = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
         let result = part1(test_case);
-        assert_eq!(result, 8i32);
+        assert_eq!(result, 41i32);
     }
 }
--- 2024/day-06/src/bin/part2.rs
+++ 2024/day-06/src/bin/part2.rs
@@ -4,8 +4,104 @@ fn main() {
     dbg!(output);
 }
 
-fn part2(_input: &str) -> i32 {
-    todo!();
+fn part2(input: &str) -> i32 {
+    let mut original_matrix: Vec<Vec<(bool, bool, bool)>> = Vec::new();
+    let mut original_current_position: (usize, usize) = (0, 0);
+    let mut original_direction: (i32, i32) = (0, 0);
+
+    for (i, l) in input.lines().enumerate() {
+        original_matrix.push(Vec::new());
+
+        for (j, c) in l.chars().enumerate() {
+            let mut obstacle = false;
+            let mut visited = false;
+
+            match c {
+                '^' => {
+                    original_direction = (-1, 0);
+                    visited = true;
+                    original_current_position = (i, j);
+                }
+                '>' => {
+                    original_direction = (0, 1);
+                    visited = true;
+                    original_current_position = (i, j);
+                }
+                'v' => {
+                    original_direction = (1, 0);
+                    visited = true;
+                    original_current_position = (i, j);
+                }
+                '<' => {
+                    original_direction = (0, -1);
+                    visited = true;
+                    original_current_position = (i, j);
+                }
+                '#' => {
+                    obstacle = true;
+                }
+                _ => {}
+            }
+
+            original_matrix[i].push((obstacle, visited, false));
+        }
+    }
+
+    let mut loop_counter = 0;
+
+    for i in 0..original_matrix.len() {
+        for j in 0..original_matrix[i].len() {
+            let mut direction = original_direction;
+            let mut current_position = original_current_position;
+            let mut matrix = original_matrix.clone();
+            if matrix[i][j].0 {
+                continue;
+            }
+            matrix[i][j].0 = true;
+
+            // Necessary for ensuring program does not count obstacles right after each other
+            let mut corner_counter = 0;
+            let mut next_step = (
+                current_position.0 as i32 + direction.0,
+                current_position.1 as i32 + direction.1,
+            );
+            // While the next square is not out of frame
+            while next_step.0 >= 0
+                && (next_step.0 as usize) < matrix.len()
+                && next_step.1 >= 0
+                && (next_step.1 as usize) < matrix[current_position.0].len()
+            {
+                // Set the current square as visited
+                matrix[current_position.0][current_position.1].1 = true;
+
+                // Check if the next square is an obstacle
+                if matrix[next_step.0 as usize][next_step.1 as usize].0 {
+                    // Check if the corner has already been visited
+                    if matrix[current_position.0][current_position.1].2 {
+                        if corner_counter == 3 {
+                            loop_counter += 1;
+                            break;
+                        }
+                        corner_counter += 1;
+                    }
+                    // Mark the square as a corner and rotate by 90 degrees
+                    matrix[current_position.0][current_position.1].2 = true;
+                    direction = (direction.1, -direction.0);
+                } else {
+                    // Update the position of the guard
+                    current_position = (next_step.0 as usize, next_step.1 as usize);
+                }
+
+                // Update the next step of the guart
+                next_step = (
+                    current_position.0 as i32 + direction.0,
+                    current_position.1 as i32 + direction.1,
+                );
+            }
+        }
+    }
+
+    loop_counter
 }
 
 #[cfg(test)]
@@ -14,8 +110,17 @@ mod tests {
 
     #[test]
     fn test1() {
-        let test_case = "8";
+        let test_case = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
         let result = part2(test_case);
-        assert_eq!(result, 8i32);
+        assert_eq!(result, 6i32);
     }
 }
